fd38237fb3de2f317e2c38e80d06c386b0e4b6b5
Hinesh Miyani <hineshmiyani@gmail.com>
2023-06-08 15:11:44 +0530

    add greet component and test

--- src/componets/Greet/Greet.test.tsx
+++ src/componets/Greet/Greet.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/Hello/);
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
-test("Greet renders with a name", () => {
-  render(<Greet name="Hinesh" />);
-  const textELement = screen.getByText("Hello Hinesh");
-  expect(textELement).toBeInTheDocument();
+describe("Nested", () => {
+  test("renders a name", () => {
+    render(<Greet name="Hinesh" />);
+    const textELement = screen.getByText("Hello Hinesh");
+    expect(textELement).toBeInTheDocument();
+  });
 });

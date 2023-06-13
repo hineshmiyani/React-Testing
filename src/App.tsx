0e603edec560c8ffd53ad00d35314a79b41addfc
Hinesh Miyani <hineshmiyani@gmail.com>
2023-06-13 09:50:12 +0530

    Add Counter component and test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Counter from "./componets/Counter/Counter";
+// import Skills from "./componets/Skills/Skills";
+// import logo from "./logo.svg";
+// import { Application } from "./componets/Application/Application";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Skills skills={["HTML", "CSS", "JAVASCRIPT"]} /> */}
+      {/* <Application /> */}
+      <Counter />
     </div>
   );
 }
---
+++ src/componets/Counter/Counter.test.tsx
@@ -0,0 +1,77 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import Counter from "./Counter";
+
+describe("Counter", () => {
+  test("renders correctly", () => {
+    render(<Counter />);
+    const counteElement = screen.getByRole("heading");
+    expect(counteElement).toBeInTheDocument();
+
+    const incrementButton = screen.getByRole("button", {
+      name: /increment/i,
+    });
+    expect(incrementButton).toBeInTheDocument();
+  });
+
+  test("rendes acount of 0", () => {
+    render(<Counter />);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("0");
+  });
+
+  test("renders a count of 1 after clicking the increment button", async () => {
+    user.setup();
+    render(<Counter />);
+    const incrementButton = screen.getByRole("button", {
+      name: /increment/i,
+    });
+    await user.click(incrementButton);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("1");
+  });
+
+  test("renders a count of 2 after clicking the increment button twice", async () => {
+    user.setup();
+    render(<Counter />);
+    const incrementButton = screen.getByRole("button", {
+      name: /increment/i,
+    });
+    await user.dblClick(incrementButton);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("2");
+  });
+
+  test("renders a count of 10 after clicking the set button", async () => {
+    user.setup();
+    render(<Counter />);
+
+    const amountInput = screen.getByRole("spinbutton");
+    await user.type(amountInput, "10");
+    expect(amountInput).toHaveValue(10);
+
+    const setButton = screen.getByRole("button", { name: /set/i });
+    await user.click(setButton);
+
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("10");
+  });
+
+  test("elemens are focused in the right order", async () => {
+    user.setup();
+    render(<Counter />);
+
+    const amountInput = screen.getByRole("spinbutton");
+    const setButton = screen.getByRole("button", { name: /set/i });
+    const incrementButton = screen.getByRole("button", { name: /increment/i });
+
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+
+    await user.tab();
+    expect(amountInput).toHaveFocus();
+
+    await user.tab();
+    expect(setButton).toHaveFocus();
+  });
+});

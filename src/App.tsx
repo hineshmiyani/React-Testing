0342bd7f1c693acdfb2f63d6882239396f516079
Hinesh Miyani <hineshmiyani@gmail.com>
2023-06-13 15:47:51 +0530

    Add MuiMode component and test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./App.css";
-import Counter from "./componets/Counter/Counter";
+import { MuiMode } from "./componets/MuiMode/MuiMode";
+import { AppProviders } from "./providers/AppProviders";
+// import Counter from "./componets/Counter/Counter";
 // import Skills from "./componets/Skills/Skills";
 // import logo from "./logo.svg";
 // import { Application } from "./componets/Application/Application";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Skills skills={["HTML", "CSS", "JAVASCRIPT"]} /> */}
-      {/* <Application /> */}
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className="App">
+        {/* <Skills skills={["HTML", "CSS", "JAVASCRIPT"]} /> */}
+        {/* <Application /> */}
+        {/* <Counter /> */}
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 

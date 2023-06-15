6bb385c1e493e13e7e96a5d0f16c2224a800242a
Hinesh Miyani <hineshmiyani@gmail.com>
2023-06-15 14:07:47 +0530

    Add users component and test and msw

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,15 @@
+// src/mocks/handlers.js
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("https://jsonplaceholder.typicode.com/users", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        { name: "Bruce Wayne" },
+        { name: "Clark Kent" },
+        { name: "Barry Allen" },
+      ])
+    );
+  }),
+];
